c6adbcbed797396d857ecb0e7f85af72670d1bcd
Oscar <Oscar>
2016-06-14 12:41:00 +0300

    Added Access Control data files

--- __openerp__.py
+++ __openerp__.py
@@ -3,19 +3,21 @@
     "summary": "Property, Units, Tenants, Occupations, Bills, Billing, Payments",
     "description": "Manages rental property",
     "author": "Oscar Koech",
-	"website":"github.com/koechoscar",
+    "website": "github.com/koechoscar",
     "application": True,
     "installable": True,
     "category": "Property",
-    "data": ['views/rental.xml',
-             'views/property.xml',
-             'views/unit.xml',
-             'views/tenant.xml',
-             'views/occupation.xml',
-             'views/bill.xml',
-             'views/payment.xml',
-             'wizard/billing.xml',
-             'report/receipt.xml'
-             ],
-    "depends": []
+    "data": [
+        'report/receipt.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/rental.xml',
+        'views/property.xml',
+        'views/unit.xml',
+        'views/tenant.xml',
+        'views/occupation.xml',
+        'views/bill.xml',
+        'views/payment.xml',
+        'wizard/billing.xml'
+    ]
 }
